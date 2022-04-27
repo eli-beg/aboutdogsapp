3d80e99935674fc8a011cb8029b8677a5cd55930
eli-beg <begaeliana@gmail.com>
2022-04-27 15:43:54 -0300

    widescreen css

--- client/src/components/WelcomeScreen.js
+++ client/src/components/WelcomeScreen.js
@@ -1,14 +1,48 @@
 import React from "react";
 import fotouno from "../../src/perrosfotos/fotouno.webp";
 import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
+import { Button, Typography } from "@mui/material";
 
 const WelcomeScreen = () => {
   return (
     <div>
-      <button className="HomeButton">
-        <Link to="/home">homee</Link>
-      </button>
-      <img className="ImgBackground" src={fotouno} alt="img not found" />
+      <Grid container direction="row" sx={{ height: "100vh" }}>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${fotouno})`,
+            backgroundRepeat: "no repeat",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5}>
+          <Box
+            sx={{
+              height: "100vh",
+              backgroundColor: "whitesmoke",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography>Welcome to About Dogs App!</Typography>
+            <Button
+              variant="outlined"
+              size="large"
+              sx={{ width: "200px", margin: "50px" }}
+            >
+              <Link to="/home" style={{ textDecoration: "none" }}>
+                homee
+              </Link>
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 };
